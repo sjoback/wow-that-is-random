8a4df40c9e3dd57c033fb2bfd93772f4685c08d3
Joakim Sjöbäck <joakim.sjoback@hotmail.com>
2022-05-02 17:16:13 +0200

    create component

---
+++ src/components/ImagePopUp/ImagePopUp.tsx
@@ -0,0 +1,40 @@
+import { useEffect, useState } from "react";
+import Overlay from "../Overlay/Overlay";
+import styles from "./ImagePopUp.module.scss";
+
+interface propTypes {
+   image: string;
+   alt: string;
+}
+
+function ImagePopUp(props: propTypes) {
+   const [open, toggleOpen] = useState(false);
+
+   // Enable ESC for closing modal
+   // useEffect(() => {
+   //    const close = (e) => {
+   //       if (e.keyCode === 27) {
+   //          toggleOpen(false);
+   //       }
+   //    };
+   //    window.addEventListener("keydown", close);
+   //    return () => window.removeEventListener("keydown", close);
+   // }, []);
+
+   return (
+      <div className={styles.container}>
+         <div className={styles.closed} onClick={() => toggleOpen(true)}>
+            <img src={props.image} alt={props.alt} />
+         </div>
+
+         {open && (
+            <div className={styles.open} onClick={() => toggleOpen(false)}>
+               <Overlay opacity={0.1} />
+               <img src={props.image} alt={props.alt} />
+            </div>
+         )}
+      </div>
+   );
+}
+
+export default ImagePopUp;
